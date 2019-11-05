759824e083cd8d097c3faa9acdd86fd2d8ba0253
adir-avraham <adir.avraham@gmail.com>
2019-11-05 15:30:54 +0200

    user-validation

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const writeToFile = require('./writeToFile')
 const fs = require('fs');
 
 const data = fs.readFileSync('users.json');
-//const users = JSON.parse(data);
-const users = [];
+const users = JSON.parse(data);
+
 
 
 api.listen(port, (err)=> {
@@ -21,7 +21,15 @@ api.listen(port, (err)=> {
 
 api.use(bodyParser.json())
 
+api.use('/', (req, res, next)=>{
 
+    const {userName, email, password, firstName, lastName} = req.body;
+    if (!userName || !email || !password || !firstName || !lastName) return res.send("Somthing is missing")
+    const isUserExist = users.find((user)=> {return user.userName === userName})
+    if (isUserExist) return res.send("User already exsited")
+    next()
+
+})
 
 api.post('/user/register', (req, res, next) => {
     
